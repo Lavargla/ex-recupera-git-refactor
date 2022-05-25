4a26bdfe5be68e0f4c4f5d8ec8a66ced9756c477
Lavargla <alvarojosegranadosvalencia@fp.iesromerovargas.com>
2022-05-25 13:29:46 +0200

    Ej1_VariablesRenombradas

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,40 +1,40 @@
-let a, b, total;
+let n1, n2, total;
 function Suma(){
     
-    a = parseInt(document.getElementById("n1").value);
-    b = parseInt(document.getElementById("n2").value);
+    n1 = parseInt(document.getElementById("n1").value);
+    n2 = parseInt(document.getElementById("n2").value);
 
-    total = a + b;
+    total = n1 + n2;
 
     window.alert(total);
 }
 
 function Resta(){
     
-    a = parseInt(document.getElementById("n1").value);
-    b = parseInt(document.getElementById("n2").value);
+    n1 = parseInt(document.getElementById("n1").value);
+    n2 = parseInt(document.getElementById("n2").value);
 
-    total = a - b;
+    total = n1 - n2;
 
     window.alert(total);
 }
 
 function Multiplicacion(){
     
-    a = parseInt(document.getElementById("n1").value);
-    b = parseInt(document.getElementById("n2").value);
+    n1 = parseInt(document.getElementById("n1").value);
+    n2 = parseInt(document.getElementById("n2").value);
     
-    total = a * b;
+    total = n1 * n2;
 
     window.alert(total);
 }
 
 function Division(){
     
-    a = parseInt(document.getElementById("n1").value);
-    b = parseInt(document.getElementById("n2").value);
+    n1 = parseInt(document.getElementById("n1").value);
+    n2 = parseInt(document.getElementById("n2").value);
 
-    total = a / b;
+    total = n1 / n2;
 
     window.alert(total);
 }
