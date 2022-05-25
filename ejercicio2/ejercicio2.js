771b5188c846476a144b65096ec49bd7a8a875aa
Lavargla <alvarojosegranadosvalencia@fp.iesromerovargas.com>
2022-05-25 14:06:45 +0200

    Ej2_Limpieza_general_2

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,18 +1,23 @@
-function pulsar(){
+function activate(){
+
     var cadena = document.getElementById("frase").value;
     var array=cadena.split(" ");
-    funcion1(array);
-    funcion2(cadena);
-    }
-    
-function funcion1(array) {
+    word_handler(array);
+    palindromo(cadena);
+
+}
+
+function word_handler(array) {
+
     var n_palabras=array.length;
     var primera_palabra=array[0];
     var ultima_palabra=array[n_palabras-1];
     document.getElementById("resultado1").innerHTML="Número de palabras: "  +n_palabras+ "<br>Primera palabra: " +primera_palabra + "<br>Ultima palabra: " +ultima_palabra;
+
 }
     
-function funcion2(cadena) {
+function palindromo(cadena) {
+
     var cadena1 = cadena.replace(/[\W_]/g, "").toLowerCase();
     var cadena2 = cadena1.split("").reverse().join("");
     
@@ -21,4 +26,5 @@ function funcion2(cadena) {
     } else {
         document.getElementById("resultado2").innerHTML="No es un palindromo"
     }
+
 }
